cdfffef856321af6ee156058a71490db7b000f35
Marouane-bichara <bicharamarouane0@gmail.com>
2024-10-29 14:21:35 +0100

    Learn foreach

--- script.js
+++ script.js
@@ -42,22 +42,82 @@
 // swap cases;
 
 
-let swappingCases = "MaRouAne";
-let invertNumbers = [1,-10,20,-30];
+// let swappingCases = "MaRouAne";
+// let invertNumbers = [1,-10,20,-30];
 
 
-let changeUpperCase =swappingCases.split('').map((e)=>
-{
-    return e == e.toUpperCase() ? e.toLowerCase() : e.toUpperCase();
-}
-).join('');
-console.log(changeUpperCase);
+// let changeUpperCase =swappingCases.split('').map((e)=>
+// {
+//     return e == e.toUpperCase() ? e.toLowerCase() : e.toUpperCase();
+// }
+// ).join('');
+// console.log(changeUpperCase);
 
-let invert = invertNumbers.map((e)=>
-{
-    return -e;
-}
-);
-console.log(invert);
+// let invert = invertNumbers.map((e)=>
+// {
+//     return -e;
+// }
+// );
+// console.log(invert);
 
 
+
+//  test map v filer
+
+// let nums = [2,1,4,57,8,4,5,6];
+
+// let evenNums = nums.filter((e)=>
+// {
+//     return e % 2 == 0;
+// }
+// )
+
+// console.log(evenNums);
+
+
+
+// let friends = ["marouane" , "ahmed" , "ali" , "khalid" , "said"];
+
+
+// let filterdfriends = friends.filter((e)=>
+// {
+//     return e.toLowerCase().startsWith('a');
+// }
+// )
+
+// console.log(filterdfriends);
+
+
+
+//  practice filter
+
+// let sentence = "I Love Foood Code Too Playing Mush"
+
+// let smallWords = sentence.split(" ").filter((e)=>
+// {
+//     return e.length <= 4;
+// }
+// ).join(" ");
+
+// console.log(smallWords);
+
+// Reduce 
+
+// let nums = [10,20,40,50];
+
+// let add = nums.reduce((acc , current , index  , array)=>
+// {
+//     console.log(`ACC => ${acc}`); 
+//     console.log(`CURRENT => ${current}`); 
+//     console.log(`INDEX => ${index}`); 
+//     console.log(`ARRAY => ${array}`); 
+    
+//     return acc + current;
+// }
+// );
+
+// console.log(add);
+
+
+
+// ForEach;
